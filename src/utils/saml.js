c3965a53807ff6f398ac996eaf74e7873bfd35b6
Jaroslaw Baczynski <jbaczynski@gmail.com>
2023-03-26 22:15:34 +0200

    SAML response processing improvements.

--- src/utils/saml.js
+++ src/utils/saml.js
@@ -2,12 +2,16 @@
 
 const JSSoup = require('jssoup').default;
 
+const REGEXP_ROLE =
+    /((.+:role\/.+),(.+:saml-provider\/.+))|((.+:saml-provider\/.+),(.+:role\/.+))/i;
+
 function getSAMLRoles(samlResponse) {
     const soup = new JSSoup(samlResponse);
     const roles = soup
         .findAll('AttributeValue')
         .filter(
             (value) =>
+                value.text &&
                 value.parent &&
                 value.parent.name === 'Attribute' &&
                 value.parent.attrs &&
@@ -15,9 +19,14 @@ function getSAMLRoles(samlResponse) {
                     'https://aws.amazon.com/SAML/Attributes/Role'
         )
         .map((value) => {
-            const [provider, role] = (value.text || '').split(',');
-            return { provider, role };
-        });
+            const [_, m1, r1, p1, m2, p2, r2] =
+                REGEXP_ROLE.exec(value.text) || [];
+            if (m1) return { provider: p1, role: r1 };
+            if (m2) return { provider: p2, role: r2 };
+            console.log('SAML: Invalid role attribute:', value.text);
+            return null;
+        })
+        .filter((value) => !!value);
     return roles;
 }
 
